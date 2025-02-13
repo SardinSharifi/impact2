be212c1e22a680b649454d83d676f1f62d6b2c25
SardinSharifi <katy.sharifi.79@gmail.com>
2025-02-13 08:48:32 +0330

    last8

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express'; 
-import * as path from 'path';
+import { Join } from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug'],
+    AppModule,
   });
 
-  // تنظیم View Engine (EJS)
-  app.setViewEngine('ejs');
-
-  // تنظیم مسیر فایل‌های Views
-  app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
-
-  // تنظیم مسیر فایل‌های استاتیک
-  app.useStaticAssets(path.join(__dirname, '..', 'public'));
-
-  // شروع به گوش دادن درخواست‌ها در پورت 3000
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   await app.listen(3000);
 }
 bootstrap();
